28c8a798d805a00af31f9c76b05701a3cfd66399
Tyler <mikroself@gmail.com>
2018-04-22 17:04:18 +0200

    make it better

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {MainNavComponent} from "./main-nav/main-nav.component";
+import {PageHeaderComponent} from "./page-header/page-header.component";
+import {PageDownloadComponent} from "./page-download/page-download.component";
+import {PageFeaturesComponent} from "./page-features/page-features.component";
+import {PageCtaComponent} from "./page-cta/page-cta.component";
+import {PageContactComponent} from "./page-contact/page-contact.component";
+import {PageFooterComponent} from "./page-footer/page-footer.component";
+import {PageMenuComponent} from "./page-menu/page-menu.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainNavComponent,
+    PageHeaderComponent,
+    PageDownloadComponent,
+    PageFeaturesComponent,
+    PageCtaComponent,
+    PageContactComponent,
+    PageFooterComponent,
+    PageMenuComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {
+}
